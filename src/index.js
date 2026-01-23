0ba067fd279516101ef6eff339d5268a2f0018d5
leonem966-rgb <leonem966@gmail.com>
2026-01-23 07:42:41 -0500

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-function generateRecipe(event) {
-    event.preventDefault();
-
-//let recipeElement = document.querySelector("#recipe");
-//recipeElement.innerHTML = "Lasagna alla Bolognese";
-
+function displayRecipe(response) {
+    console.log("recipe generated");
 new Typewriter("#recipe", {
-    strings: ["Lasagna alla Bolognese"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 75,
     cursor: "",
@@ -13,5 +9,25 @@ new Typewriter("#recipe", {
 
 }
 
+
+function generateRecipe(event) {
+    event.preventDefault();
+
+
+let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "abtb6a92443361bbdo4b8fd76decc014";
+let prompt = `User instructions: Generate an Italian recipe about ${instructionsInput.value}`;
+let context = "You are an expert in Italian recipes. You love to write short Italian recipes with eight ingredients or less. Your mission is to generate an Italian recipes with 8 lines of ingredients or less in basic HTML. Make sure to follow the user intstructions. Sign the bottom of the recipe with SheCodes AI.";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log ("Generating recipe");
+console.log('Prompt: ${prompt}');
+console.log('Context: ${context}');
+
+axios.get(apiUrl).then(displayRecipe); 
+}
+
+
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
+
